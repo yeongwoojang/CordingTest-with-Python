ea73c13f74c239a0f306153eedf2e2e0e099f273
yeongwoojang <52729223+yeongwoojang@users.noreply.github.com>
2021-01-18 01:32:07 +0900

    Create 문자열 자료형.py

---
+++ 파이썬문법/문자열 자료형.py
@@ -0,0 +1,19 @@
+# 1. 문자열 초기화
+data = 'Hello World'
+print(data)
+
+data = "Dont`t you know \"Python\"?"
+# data = "Dont`t you know 'Python'?"
+print(data)
+
+# 2. 문자열 연산
+a= "Hello"
+b = "world"
+
+print(a+" "+b)
+
+a = "String"
+print(a*3)
+
+a= "ABCDE"
+print(a[2 : 4])
